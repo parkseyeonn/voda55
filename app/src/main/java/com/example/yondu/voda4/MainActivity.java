7fd64b9b92022cff149c5cb71f93921c10c96b1a
parkseyeonn <yondu0902@gmail.com>
2018-05-07 20:28:19 +0900

    sample

---
+++ app/src/main/java/com/example/yondu/voda4/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yondu.voda4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View v){
+        Intent intent = new Intent(getApplicationContext(), Menu1Activity.class);
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/yondu/voda4/Menu1Activity.java
@@ -0,0 +1,55 @@
+package com.example.yondu.voda4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.github.florent37.materialviewpager.MaterialViewPager;
+
+public class Menu1Activity extends AppCompatActivity {
+
+    private MaterialViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+
+        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+
+        //viewpager
+        ViewPager viewPager = mViewPager.getViewPager();
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+        });
+
+        //After set an adapter to the ViewPager
+        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
+        //toolbar
+        Toolbar toolbar = mViewPager.getToolbar();
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(false);
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+}
+
